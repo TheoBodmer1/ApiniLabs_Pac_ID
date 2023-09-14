bed9a90c632d7b1a4f5aeb93fcc8861ce6b0a321
TheoBodmer1 <theo.bodmer@insafety.ch>
2023-09-14 09:05:08 -0300

    first commit - commiting all files to the repository

---
+++ example/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pkg/flutter_pkg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Theo Test Local Package',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const MyHomePage(title: 'Theo Test Local Package'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            CustomButton(
+                onPressed: _incrementCounter,
+                child: const Text("Custom Button via Local Package")),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
