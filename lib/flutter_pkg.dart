e92ef2a4129124aab49ccd23051deaafaede5662
TheoBodmer1 <145011478+TheoBodmer1@users.noreply.github.com>
2023-09-14 17:24:29 -0300

    Update flutter_pkg.dart
    Change button color to red

--- lib/flutter_pkg.dart
+++ lib/flutter_pkg.dart
@@ -17,7 +17,7 @@ class CustomButton extends StatelessWidget {
         style: TextButton.styleFrom(
           padding: const EdgeInsets.all(16.0),
           primary: Colors.white,
-          backgroundColor: Colors.yellow,
+          backgroundColor: Colors.red,
           elevation: 9.0,
           textStyle: const TextStyle(
             fontSize: 20,
